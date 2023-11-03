63ac72abecee08d3d323d27c742430760811ed04
Aarni Halinen <aarni.halinen@gmail.com>
2023-11-03 21:43:07 +0200

    chore(CI): add flags for audit

--- aarnihalinen.fi/astro.config.mjs
+++ aarnihalinen.fi/astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), react()]
+  integrations: [mdx(), sitemap(), react()],
 });
