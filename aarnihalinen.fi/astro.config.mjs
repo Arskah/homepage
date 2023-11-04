d2e966703fefaccfe6f73e0bae57c215178212c8
Aarni Halinen <aarni.halinen@gmail.com>
2023-11-04 08:07:54 +0200

    chore: documnet issue in config

--- aarnihalinen.fi/astro.config.mjs
+++ aarnihalinen.fi/astro.config.mjs
@@ -4,8 +4,15 @@ import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
 
+// https://github.com/withastro/astro/issues/4432
+// const isDev = process.env.NODE_ENV === "development";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), react()],
+  // vite: {
+  //   legacy: { buildSsrCjsExternalHeuristics: !isDev },
+  //   ssr: { noExternal: ["styled-components"] },
+  // },
 });
