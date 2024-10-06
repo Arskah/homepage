60eb5fc58f71ed6fbd7ab658028b9860aff9d989
Aarni Halinen <Arskah@users.noreply.github.com>
2024-10-06 21:15:54 +0300

    fix: recreate astro blog template (#324)

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://aarnihalinen.fi",
+  integrations: [mdx(), react(), sitemap()],
+});
