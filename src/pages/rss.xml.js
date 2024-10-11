c57536169d61358d888ff2d52922a9acc0ac77cb
Aarni Halinen <aarni.halinen@gmail.com>
2024-10-12 01:17:52 +0300

    chore: fix issues

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,4 @@
 import rss from "@astrojs/rss";
-// eslint-disable-next-line import-x/no-unresolved
 import { getCollection } from "astro:content";
 
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
@@ -7,12 +6,12 @@ import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
     description: SITE_DESCRIPTION,
-    site: context.site,
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
     })),
+    site: context.site,
+    title: SITE_TITLE,
   });
 }
