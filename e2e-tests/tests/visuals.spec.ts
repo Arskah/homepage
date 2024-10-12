38ed1033d0c47e71fd7663f0ba94364d57d6c4e3
Aarni Halinen <Arskah@users.noreply.github.com>
2024-10-12 23:17:15 +0300

    chore: take fullpage screenshots (#363)

--- e2e-tests/tests/visuals.spec.ts
+++ e2e-tests/tests/visuals.spec.ts
@@ -2,15 +2,15 @@ import { expect, test } from "@playwright/test";
 
 test("index page", async ({ page }) => {
   await page.goto("/");
-  await expect(page).toHaveScreenshot("index.png");
+  await expect(page).toHaveScreenshot("index.png", { fullPage: true });
 });
 
 test("blog page", async ({ page }) => {
   await page.goto("/blog");
-  await expect(page).toHaveScreenshot("blog.png");
+  await expect(page).toHaveScreenshot("blog.png", { fullPage: true });
 });
 
 test("about page", async ({ page }) => {
   await page.goto("/about");
-  await expect(page).toHaveScreenshot("about.png");
+  await expect(page).toHaveScreenshot("about.png", { fullPage: true });
 });
