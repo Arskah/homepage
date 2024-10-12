e6f6ed13bfcfdc26c20111dd145c624da919d253
Aarni Halinen <aarni.halinen@gmail.com>
2024-10-12 16:40:31 +0300

    chore: change CSS libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react(), sitemap()],
   site: "https://aarnihalinen.fi",
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+  },
 });
