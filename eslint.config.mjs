1e5295ab0b63c072760d6697725c7baee96af214
Aarni Halinen <git@aarnihalinen.fi>
2025-10-04 20:29:52 +0300

    chore: new playwright config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -71,12 +71,7 @@ export default defineConfig([
   },
   {
     files: ["tests/**"],
-    plugins: {
-      playwright: playwright,
-    },
-    rules: {
-      ...playwright.configs.recommended.rules,
-    },
+    extends: [playwright.configs["flat/recommended"]],
   },
   {
     files: ["**/*.md"],
